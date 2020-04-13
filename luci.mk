efb77f996fda89b17b210fc71284d8b3bc214575
osdu <858908467@qq.com>
2020-04-13 17:55:09 +0800

    Update luci.mk

--- luci.mk
+++ luci.mk
@@ -188,9 +188,9 @@ define Package/$(PKG_NAME)/install
 	if [ -d $(PKG_BUILD_DIR)/htdocs ]; then \
 	  $(INSTALL_DIR) $(1)$(HTDOCS); \
 	  cp -pR $(PKG_BUILD_DIR)/htdocs/* $(1)$(HTDOCS)/; \
-	  if [ -d $(PKG_BUILD_DIR)/htdocs/luci-static ]; then \
-		(cd $(1)$(HTDOCS)/assets && find -type f -name '*.less' -print0 |xargs -0 -r rm); \
-		(cd $(1)$(HTDOCS)/luci-static && find -type f -name '*.less' -print0 |xargs -0 -r rm); \
+	  if [ -d $(PKG_BUILD_DIR)/htdocs ]; then \
+		(cd $(1)$(HTDOCS)/assets 2>/dev/null && find -type f -name '*.less' -print0 |xargs -0 -r rm || true); \
+		(cd $(1)$(HTDOCS)/luci-static 2>/dev/null && find -type f -name '*.less' -print0 |xargs -0 -r rm || true); \
 	  fi; \
 	else true; fi
 	if [ -d $(PKG_BUILD_DIR)/root ]; then \
